416fbf83086567228e6add2ae18067e3d19ea990
Bassam Husain <61985779+bassamanator@users.noreply.github.com>
2022-10-03 00:12:17 -0400

    Initial commit local.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { About } from './components/About';
+import { FeaturedProducts } from './components/FeaturedProducts';
+import { Home } from './components/Home';
+import { Navbar } from './components/Navbar';
+import { NewProducts } from './components/NewProducts';
+import { NoMatch } from './components/NoMatch';
+import { Products } from './components/Products';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Navbar></Navbar>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='about' element={<About />} />
+
+				<Route path='products' element={<Products />}>
+					<Route path='featured' element={<FeaturedProducts />} />
+					<Route path='new' element={<NewProducts />} />
+				</Route>
+				<Route path='*' element={<NoMatch />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
